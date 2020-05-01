d1585efa87cec47509d492ab836759d3049754b1
Kevin <kevin.yi93@gmail.com>
2020-05-01 17:17:51 -0400

    Call getuser and getquestion functions in data initialization

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,8 @@ import {
 
 export function getInitialData() {
   return Promise.all([
-    _getUsers,
-    _getQuestions,
+    _getUsers(),
+    _getQuestions(),
   ]).then(([users, questions]) => ({
     users,
     questions,
