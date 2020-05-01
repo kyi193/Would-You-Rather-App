3b7699b30301528f53049f548f89af95573c2d9b
Kevin <kevin.yi93@gmail.com>
2020-05-01 16:16:39 -0400

    Attach middleware to store

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import middleware from './middleware';
 
-const createStore = createStore(reducer)
+const createStore = createStore(reducer, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
