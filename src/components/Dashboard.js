29d127301b15690e3f81dd3dea39da194bd274ec
Kevin <kevin.yi93@gmail.com>
2020-05-03 17:00:59 -0400

    Display list of user's unanswered questions ids

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,21 +7,32 @@ class Dashboard extends Component {
     return (
       <div>
         <ul className='dashboard-list'>
-          {this.props.questionIDs.map((id) => (
-            <li key={id}>
-              <div>QUESTION ID: {id}</div>
-            </li>
-          ))}
+          {this.props.unAnsweredQuestionIDs &&
+            (this.props.unAnsweredQuestionIDs.map((id) => (
+              <li key={id}>
+                <div>QUESTION ID: {id}</div>
+              </li>
+            )))}
         </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps({ questions }) {
-  return {
-    questionIDs: Object.keys(questions)
+function mapStateToProps({ questions, authedUser, users }) {
+  let unansweredQuestionKeys;
+  if (users[authedUser]) {
+    const answeredQuestionKeys = Object.keys(users[authedUser].answers)
+    const questionKeys = Object.keys(questions)
       .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+    unansweredQuestionKeys = questionKeys.filter(question => (
+      !answeredQuestionKeys.includes(question)
+    ))
+
+  }
+
+  return {
+    unAnsweredQuestionIDs: unansweredQuestionKeys ? unansweredQuestionKeys : null
   }
 }
 
