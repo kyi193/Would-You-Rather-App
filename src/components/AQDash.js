cc4f2f9de9a3bafa42d002dd1410de01865e03c9
Kevin <kevin.yi93@gmail.com>
2020-05-04 12:30:38 -0400

    Display user's selected answer in the user's answered question page of the dashboard

--- src/components/AQDash.js
+++ src/components/AQDash.js
@@ -17,24 +17,45 @@ class AQDash extends Component {
     const isOptionOne = question.optionOne.votes.includes(authedUser)
     const isOptionTwo = question.optionTwo.votes.includes(authedUser)
     console.log(optionOne, optionTwo, isOptionOne, isOptionTwo)
-    return (
-      <div className="unanswered-questionDash">
-        <img
-          src={avatarURL}
-          alt={`Avatar of ${name}`}
-          className='avatar'
-        />
-        <div>{name} Asks</div>
-        <div>Would you rather...</div>
-        <div>
-          A. {optionOne.text}
-          <br />
+    if (isOptionOne) {
+      return (
+        <div className="unanswered-questionDash">
+          <img
+            src={avatarURL}
+            alt={`Avatar of ${name}`}
+            className='avatar'
+          />
+          <div>{name} Asks</div>
+          <div>Would you rather...</div>
+          <div>
+            A. {optionOne.text} - <span><strong>You chose this answer!</strong></span>
+            <br />
           Or.....
           <br />
           B. {optionTwo.text}
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else if (isOptionTwo) {
+      return (
+        <div className="unanswered-questionDash">
+          <img
+            src={avatarURL}
+            alt={`Avatar of ${name}`}
+            className='avatar'
+          />
+          <div>{name} Asks</div>
+          <div>Would you rather...</div>
+          <div>
+            A. {optionOne.text}
+            <br />
+          Or.....
+          <br />
+          B. {optionTwo.text} - <span><strong>You chose this answer!</strong></span>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
